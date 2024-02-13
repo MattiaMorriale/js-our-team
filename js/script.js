9539882635098d2f0c8f5eaa2ab534c798076d48
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-13 15:42:55 +0100

    mod

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-const team = {
-    nome:['Wayne Barnett','Angela Caroll','Walter Gordon','Angela Lopez','Scott Estrada','Barbara Ramos'],
-    ruolo:['Founder & CEO','Chief Editor','Office Manager','Social Media Manager','Developer','Graphic Designer'],
-    foto:['wayne-barnett-founder-ceo.jpg','angela-caroll-chief-editor.jpg','walter-gordon-office-manager.jpg','angela-lopez-social-media-manager.jpg','scott-estrada-developer.jpg','barbara-ramos-graphic-designer.jpg']
-}
+const team = [
+{
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    foto: "wayne-barnett-founder-ceo.jpg",
+},
 
-console.log(team)
+{
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    foto: "walter-gordon-office-manager.jpg",
+},
+{
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+    foto: "walter-gordon-office-manager.jpg",
+},
+{
+    nome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+    foto: "angela-lopez-social-media-manager.jpg",
+},
+{
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+    foto: "scott-estrada-developer.jpg",
+},
+{
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    foto: "barbara-ramos-graphic-designer.jpg",
+}
+];
+
+
+
+for (let i = 0; i < team.length; i++) {
+    console.log(team[i])
+}
