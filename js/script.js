36429348ebcaa8715bb4a8d60d9f61c2c48d94b4
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-13 15:29:59 +0100

    fatto minestone 0

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const team = {
+    nome:['Wayne Barnett','Angela Caroll','Walter Gordon','Angela Lopez','Scott Estrada','Barbara Ramos'],
+    ruolo:['Founder & CEO','Chief Editor','Office Manager','Social Media Manager','Developer','Graphic Designer'],
+    foto:['wayne-barnett-founder-ceo.jpg','angela-caroll-chief-editor.jpg','walter-gordon-office-manager.jpg','angela-lopez-social-media-manager.jpg','scott-estrada-developer.jpg','barbara-ramos-graphic-designer.jpg']
+}
+
+console.log(team)
